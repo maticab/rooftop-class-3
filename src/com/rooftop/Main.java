06b3a472935114457c4a580e989ad524be598bbf
maticab <maticab@gmail.com>
2022-04-04 20:31:21 -0300

    Initial commit

---
+++ src/com/rooftop/Main.java
@@ -0,0 +1,10 @@
+package com.rooftop;
+
+public class Main {
+    public static void main(String[] args) {
+           System.out.println(args[0]);System.out.println(args[1]);
+        for (int as = 0; as < 100; as++) {
+
+        }
+    }
+}
